6d367df316f096436389f5662e5b540cc0d04449
Julian Zimmermann <JulZimmermann@gmail.com>
2019-03-25 13:40:45 +0100

    blink

--- Exercise3/Part1/main.cpp
+++ Exercise3/Part1/main.cpp
@@ -1,5 +1,47 @@
 
+#include <avr/io.h>
+#include <stddef.h>
+
+constexpr int ALL_OUTPUT = 0xff;
+constexpr int ALL_INPUT = 0;
+
+int blink();
+void led(size_t count);
+
 int main() {
+    DDRD = ALL_INPUT;
+    DDRB = ALL_OUTPUT;
+
+    PORTB = 0xff;
+    PIND = 0;
+
+    blink();
 
     return 0;
+}
+
+int blink() {
+    size_t count = 0;
+    bool isPressed = false;
+
+    while (true) {
+        if (!isPressed && !PIND) {
+            ++count;
+            isPressed = true;
+
+            led(count);
+        }
+
+        if (PIND) {
+            isPressed = false;
+        }
+    }
+}
+
+void led(size_t count) {
+    auto value = static_cast<uint8_t>(count % 8);
+
+    value = ~value;
+
+    PORTB = value;
 }

49293421656190d952d0706b13790f7692ecde7a
Julian Zimmermann <JulZimmermann@gmail.com>
2019-03-26 10:27:16 +0100

    old

--- Exercise3/Part1/main.cpp
+++ Exercise3/Part1/main.cpp
@@ -1,20 +1,47 @@
 
 #include <avr/io.h>
+#include <stddef.h>
 
-int led() {
+constexpr int ALL_OUTPUT = 0xff;
+constexpr int ALL_INPUT = 0;
 
-}
+int blink();
+void led(size_t count);
 
 int main() {
-    while (1) {
-        led();
-        DDRD = 0;
-        DDRB = 0xff;
+    DDRD = ALL_INPUT;
+    DDRB = ALL_OUTPUT;
 
-        PORTB = 0;
-        PIND = 0;
-    }
+    PORTB = 0;
+    PIND = 0;
+
+    //blink();
 
     return 0;
 }
 
+int blink() {
+    size_t count = 0;
+    bool isPressed = false;
+
+    while (true) {
+        if (!isPressed && !PIND) {
+            ++count;
+            isPressed = true;
+
+            led(count);
+        }
+
+        if (PIND) {
+            isPressed = false;
+        }
+    }
+}
+
+void led(size_t count) {
+    auto value = static_cast<uint8_t>(count % 8);
+
+    value = ~value;
+
+    PORTB = value;
+}

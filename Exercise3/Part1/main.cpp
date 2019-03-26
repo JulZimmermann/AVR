c25cc54ebf238e518bd1de5c631e81fd847e9b3d
Julian Zimmermann <JulZimmermann@gmail.com>
2019-03-26 10:24:03 +0100

    fixed cmake

--- Exercise3/Part1/main.cpp
+++ Exercise3/Part1/main.cpp
@@ -1,47 +1,20 @@
 
 #include <avr/io.h>
-#include <stddef.h>
 
-constexpr int ALL_OUTPUT = 0xff;
-constexpr int ALL_INPUT = 0;
+int led() {
 
-int blink();
-void led(size_t count);
+}
 
 int main() {
-    DDRD = ALL_INPUT;
-    DDRB = ALL_OUTPUT;
+    while (1) {
+        led();
+        DDRD = 0;
+        DDRB = 0xff;
 
-    PORTB = 0xff;
-    PIND = 0;
-
-    blink();
+        PORTB = 0;
+        PIND = 0;
+    }
 
     return 0;
 }
 
-int blink() {
-    size_t count = 0;
-    bool isPressed = false;
-
-    while (true) {
-        if (!isPressed && !PIND) {
-            ++count;
-            isPressed = true;
-
-            led(count);
-        }
-
-        if (PIND) {
-            isPressed = false;
-        }
-    }
-}
-
-void led(size_t count) {
-    auto value = static_cast<uint8_t>(count % 8);
-
-    value = ~value;
-
-    PORTB = value;
-}

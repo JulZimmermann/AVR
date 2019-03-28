58a662fd64e8f8557fbf8ca25a21f19ad649e44e
Julian Zimmermann <JulZimmermann@gmail.com>
2019-03-28 08:12:37 +0100

    typo

--- FlashableExecutable.cmake
+++ FlashableExecutable.cmake
@@ -1,8 +1,8 @@
-if(NOT SERIAL_FLUSH_PORT)
-    set(PORT /dev/ttyUSB0 CACHE STRING "Port for flushing the programm")
+if(NOT SERIAL_FLASH_PORT)
+    set(PORT /dev/ttyUSB0 CACHE STRING "Port for flashing the programm")
 endif()
 
-function(add_flushable_executable)
+function(add_flashable_executable)
     add_executable(${ARGV})
 
     add_custom_command(TARGET ${ARGV0} POST_BUILD
@@ -10,6 +10,6 @@ function(add_flushable_executable)
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
             COMMENT "Creating hex file")
 
-    add_custom_target(${ARGV0}_flash COMMAND avrdude -p m32 -c stk500 -U flash:w:${ARGV0}.hex -P ${SERIAL_FLUSH_PORT}
+    add_custom_target(${ARGV0}_flash COMMAND avrdude -p m32 -c stk500 -U flash:w:${ARGV0}.hex -P ${SERIAL_FLASH_PORT}
             DEPENDS ${ARGV0})
 endfunction()

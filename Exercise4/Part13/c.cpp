83831986d8733fac4c1d0066fbccf819ef693c85
konrad melzer <konrad.melzer@kurzdigital.com>
2019-04-01 13:05:13 +0200

    Clock.h

--- Exercise4/Part13/c.cpp
+++ Exercise4/Part13/c.cpp
@@ -1,10 +1,13 @@
 
 #include <avr/io.h>
 #include <stddef.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <Clock.h>
 
 constexpr int ALL_HIGH = 0xFF;
+
 
 uint8_t count = 0;
 
@@ -16,7 +19,7 @@ int main() {
     TIMSK |= (1<<OCIE1A);                    // Enable Timer 0 compare interrupt
     TCCR1B |= (1<<WGM12)|(1<<CS12)|(1<<CS10);           // Start timer0 with prescaler 1024
 
-    OCR1A = (uint16_t) ((F_CPU/1024) - 1) * 8;
+    OCR1A = calcComp<uint16_t>(1024, 1000);
 
     sei();                               // Set the I-bit in SREG
 
---
+++ Util/Clock.h
@@ -0,0 +1,15 @@
+//
+// Created by konrad on 01.04.19.
+//
+
+#ifndef AVR_CLOCK_H
+#define AVR_CLOCK_H
+
+constexpr uint32_t T_CPU = 8000000;
+
+template<typename T>
+constexpr T calcComp(uint16_t prescaler, size_t millis) {
+    return (T_CPU / (prescaler * (millis / 1000))) - 1;
+}
+
+#endif //AVR_CLOCK_H
